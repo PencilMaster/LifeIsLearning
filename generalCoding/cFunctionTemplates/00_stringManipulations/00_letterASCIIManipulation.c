8ffe5a376b7303cb3206f66ce971def164292e11
PencilMaster <lennard.friedrich@gmail.com>
2025-06-21 15:09:53 +0200

    Added coding directory in which personal projects and everything with code, that does not belong to a specific subject, will find its place. \n added first little C function template as well, it takes in an int* and char* and counts all occurences of each lowercase letter in the string.

---
+++ generalCoding/cFunctionTemplates/00_stringManipulations/00_letterASCIIManipulation.c
@@ -0,0 +1,29 @@
+// Here I will write and store C functions, which manipulate string inputs.
+// We will handle functions that do not return allocated memory for now. 
+// Those will need to be handled with much care and memory needs to be freed by the user.
+//
+
+#ifndef LETTERASCIIMANIPULATION.C
+#define LETTERASCIIMANIPULATION.C
+
+
+// ========== This block will count characters in a given string ==========
+#include <stdlib.h>
+#include <string.h>
+
+void countCharacters(int* countChar, char* str) {
+  size_t strLength = strlen(str);
+  for (size_t i = 0; i < strLength; ++i) {
+    ++countChar[str[i] - 'a'];
+  }
+  return;
+}
+// ========================================================================
+
+
+
+
+
+
+
+#endif
