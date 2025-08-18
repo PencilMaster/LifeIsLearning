3c68edef31563ffd06ee737aa1bcda6d00ec563c
PencilMaster <lennard.friedrich@gmail.com>
2025-08-18 09:55:24 +0200

    In 02_socketAndBind.c we now only include needed libraries with POSIX. For old unix machines, using _POSIX_C_SOURCE 200112L might be needed.

--- projects/servers/httpServer/tryoutAlgorithms/02_socketAndBind.c
+++ projects/servers/httpServer/tryoutAlgorithms/02_socketAndBind.c
@@ -2,13 +2,12 @@
 //
 //
 
+#define _POSIX_C_SOURCE 200809L
 #include <stdio.h>
 #include <string.h>
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <netdb.h>
-#include <arpa/inet.h>
-#include <netinet/in.h>
 
 int main(int argc, char *argv[]) 
 {
